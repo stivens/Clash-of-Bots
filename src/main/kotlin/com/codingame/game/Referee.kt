53d61fbcd04ff81f1cede82e157a258d5130b6b0
stivens <jacekbizub@gmail.com>
2021-03-18 21:42:09 +0100

    init Player and Referee

--- src/main/kotlin/com/codingame/game/Referee.kt
+++ src/main/kotlin/com/codingame/game/Referee.kt
@@ -1,4 +1,18 @@
 package com.codingame.game
 
-class Referee {
+import com.codingame.gameengine.core.AbstractReferee
+import com.codingame.gameengine.core.MultiplayerGameManager
+import com.google.inject.Inject
+
+class Referee : AbstractReferee() {
+    @Inject
+    private lateinit var gameManager: MultiplayerGameManager<Player>
+
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+
+    override fun gameTurn(turn: Int) {
+        TODO("Not yet implemented")
+    }
 }
