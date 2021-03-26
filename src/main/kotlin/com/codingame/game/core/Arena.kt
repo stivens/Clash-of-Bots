7fc51b5c8a5c92ba2baaf2f27f10b4cb276ea0b8
stivens <jacekbizub@gmail.com>
2021-03-26 14:04:09 +0100

    reorganize code

--- src/main/kotlin/com/codingame/game/core/Arena.kt
+++ src/main/kotlin/com/codingame/game/core/Arena.kt
@@ -2,31 +2,6 @@ package com.codingame.game.core
 
 import com.codingame.game.Player
 
-data class Position(val x: Int, val y: Int) {
-    fun allNeighborsInRange(range: Int): List<List<Position>> =
-        (y - range .. y + range).map { neighborY ->
-            (x - range .. x + range).map { neighborX ->
-                Position(neighborX, neighborY)
-            }
-        }
-
-    fun normalizeOverflow(width: Int, height: Int): Position {
-        val xWithOverflow =
-            when {
-                x < 0 -> width + x
-                x >= width -> x % width
-                else -> x
-            }
-        val yWithOverflow = when {
-            y < 0 -> height + y
-            y >= height -> y % height
-            else -> y
-        }
-
-        return Position(xWithOverflow, yWithOverflow)
-    }
-}
-
 data class Arena constructor(
     val width: Int,
     val height: Int,
@@ -50,5 +25,30 @@ data class Arena constructor(
     fun putNewRobot(robot: Robot, position: Position) {
         val (x, y) = position.normalizeOverflow(width, height)
         board[y][x] = robot
+    }
+}
+
+data class Position(val x: Int, val y: Int) {
+    fun allNeighborsInRange(range: Int): List<List<Position>> =
+        (y - range .. y + range).map { neighborY ->
+            (x - range .. x + range).map { neighborX ->
+                Position(neighborX, neighborY)
+            }
+        }
+
+    fun normalizeOverflow(width: Int, height: Int): Position {
+        val xWithOverflow =
+            when {
+                x < 0 -> width + x
+                x >= width -> x % width
+                else -> x
+            }
+        val yWithOverflow = when {
+            y < 0 -> height + y
+            y >= height -> y % height
+            else -> y
+        }
+
+        return Position(xWithOverflow, yWithOverflow)
     }
 }
