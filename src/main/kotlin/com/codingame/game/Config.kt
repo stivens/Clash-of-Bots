3b20d444e2e78b33d8b4541835258c3cc51ecac3
stivens <jacekbizub@gmail.com>
2021-04-08 16:50:55 +0200

    add Config object

---
+++ src/main/kotlin/com/codingame/game/Config.kt
@@ -0,0 +1,11 @@
+package com.codingame.game
+
+object Config {
+    object Robots {
+        const val MAX_HEALTH: Int = 100
+        const val ATTACK_DAMAGE: Int = 20
+        const val EXPLOSION_DAMAGE: Int = 10
+        const val EXPLOSION_RANGE: Int = 2
+        const val GUARD_MODIFIER: Double = 0.5
+    }
+}
