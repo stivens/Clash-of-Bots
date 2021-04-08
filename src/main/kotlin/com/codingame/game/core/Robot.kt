b1713ecc330bb800e465f22415eee68aaead005b
stivens <jacekbizub@gmail.com>
2021-04-08 14:39:55 +0200

    add UUID for Robot class

--- src/main/kotlin/com/codingame/game/core/Robot.kt
+++ src/main/kotlin/com/codingame/game/core/Robot.kt
@@ -1,10 +1,12 @@
 package com.codingame.game.core
 
 import com.codingame.game.Player
+import java.util.*
 
 data class Robot(
     val owner: Player,
     val health: Int,
+    val uuid: UUID = UUID.randomUUID()
 ) {
     val isAlive: Boolean = health > 0
     val isNotAlive: Boolean = !isAlive
