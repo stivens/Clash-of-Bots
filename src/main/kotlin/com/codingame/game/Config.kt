d1aa25318f4c9adef87c0782e43044ab1be50f19
stivens <jacekbizub@gmail.com>
2021-04-16 11:04:44 +0200

    Modify config

--- src/main/kotlin/com/codingame/game/Config.kt
+++ src/main/kotlin/com/codingame/game/Config.kt
@@ -2,11 +2,11 @@ package com.codingame.game
 
 object Config {
     object Robots {
-        const val MAX_HEALTH: Int = 100
-        const val ATTACK_DAMAGE: Int = 20
-        const val COLLISION_DAMAGE: Int = 5
-        const val EXPLOSION_DAMAGE: Int = 40
-        const val EXPLOSION_RANGE: Int = 2
+        const val MAX_HEALTH: Int = 10
+        const val ATTACK_DAMAGE: Int = 2
+        const val COLLISION_DAMAGE: Int = 1
+        const val EXPLOSION_DAMAGE: Int = 4
+        const val EXPLOSION_RANGE: Int = 1
         const val GUARD_MODIFIER: Double = 0.5
     }
 }
