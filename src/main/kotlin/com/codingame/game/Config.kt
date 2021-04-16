19d605b527781ef866a9214d2860029ae6df2e8e
stivens <jacekbizub@gmail.com>
2021-04-16 10:19:02 +0200

    Change explosion damage

--- src/main/kotlin/com/codingame/game/Config.kt
+++ src/main/kotlin/com/codingame/game/Config.kt
@@ -5,7 +5,7 @@ object Config {
         const val MAX_HEALTH: Int = 100
         const val ATTACK_DAMAGE: Int = 20
         const val COLLISION_DAMAGE: Int = 5
-        const val EXPLOSION_DAMAGE: Int = 10
+        const val EXPLOSION_DAMAGE: Int = 40
         const val EXPLOSION_RANGE: Int = 2
         const val GUARD_MODIFIER: Double = 0.5
     }
